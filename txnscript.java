a3ce28e3e62cdf7cb7e21c89b3913189160668d3
othmane-mattiche <63346392+othmanematt@users.noreply.github.com>
2020-05-08 03:31:46 +0000

    Update txnscript.java

--- txnscript.java
+++ txnscript.java
@@ -144,8 +144,6 @@ public class txnscript
 		try
 		{
 				PreparedStatement pstmt = cnx.prepareStatement(sql) ;
-				pstmt.setString(1, nom);
-				pstmt.setDouble(2, codePostal);
 				pstmt.executeUpdate();
 		}
 		catch (SQLException e)
